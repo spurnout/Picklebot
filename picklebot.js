9a0f0a620ac0a98f4a921d6af9b5b5bee2917e20
spurnout <spurnout@gmail.com>
2020-05-28 23:06:08 -0700

    Update picklebot.js

--- picklebot.js
+++ picklebot.js
@@ -5,13 +5,15 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-newUserCheck = 
-\\code to check if a user account is newly made within 5 minutes and if so to ban them
+
+bot.on('guildMemberAdd', member => {
+    \\code to check if a user account is newly made within 5 minutes and if so to ban them
 if (Date.now() - user.CreatedAt <= 300) {
 	guildMember.ban({ days: 14, reason: 'New Account' })
 	.then(console.log)
 	.catch(console.error);
-}
+
+}}
 \\ still need to encapsulate this if statement above in a check for when new users join the server
 	
 
